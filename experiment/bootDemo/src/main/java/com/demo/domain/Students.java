86ee2055b72c821aa8f36bd91906758785578645
MixJade <1114185977@qq.com>
2022-11-25 23:46:08 +0800

    MixJade

--- experiment/bootDemo/src/main/java/com/demo/domain/Students.java
+++ experiment/bootDemo/src/main/java/com/demo/domain/Students.java
@@ -3,9 +3,8 @@ package com.demo.domain;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import lombok.Data;
 
-@Data
+
 public class Students {
     @TableField("studentName")
     String studentName;
@@ -21,4 +20,76 @@ public class Students {
     Integer societyId;
     Integer money;
     double height;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Integer getEnglishGrade() {
+        return englishGrade;
+    }
+
+    public void setEnglishGrade(Integer englishGrade) {
+        this.englishGrade = englishGrade;
+    }
+
+    public Integer getMathGrade() {
+        return mathGrade;
+    }
+
+    public void setMathGrade(Integer mathGrade) {
+        this.mathGrade = mathGrade;
+    }
+
+    public Integer getSocietyId() {
+        return societyId;
+    }
+
+    public void setSocietyId(Integer societyId) {
+        this.societyId = societyId;
+    }
+
+    public Integer getMoney() {
+        return money;
+    }
+
+    public void setMoney(Integer money) {
+        this.money = money;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
