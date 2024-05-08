d66270e9761bbd95796deb533ab09892b8c28dcd
MixJade <1114185977@qq.com>
2024-05-08 17:36:31 +0800

    JackSon将String转为List_obj

---
+++ experiment/bootDemo/src/test/java/com/demo/TesStringToListObj.java
@@ -0,0 +1,42 @@
+package com.demo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * JackSon将String转为List_obj
+ *
+ * @author MixJade
+ * @since 2024-5-8 17:33:45
+ */
+public class TesStringToListObj {
+    public static void main(String[] args) {
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("张三", 13));
+        personList.add(new Person("李四", 24));
+        personList.add(new Person("王五", 15));
+
+        // 获取JackSon对象
+        ObjectMapper om = new ObjectMapper();
+        try {
+            // 将List_obj转为String
+            String jsonString = om.writeValueAsString(personList);
+            System.out.println("转为String的结果:" + jsonString);
+
+            // 将String转为List_obj，注意这里的接收类型List<Person>是默认转化目标
+            List<Person> listPerson = om.readValue(jsonString, new TypeReference<>() {
+            });
+            System.out.println("转成的结果：" + listPerson);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    record Person(String name, int age) {
+    }
+}
