cd2aadab5cc43cf524568732c80f1abd8029634e
MixJade <111669273+MixJade@users.noreply.github.com>
2024-06-06 22:37:00 +0800

    流程图破碎节点连线新增节点排序功能

--- codeLearning/src/main/java/leetCode/CutFlowLine.java
+++ codeLearning/src/main/java/leetCode/CutFlowLine.java
@@ -6,7 +6,9 @@ import java.util.stream.Collectors;
 /**
  * 破碎节点连线
  * 有一个流程图，它的一些连线上的节点被扣掉了
- * 现在需要重新连线(要求把破碎的线接上)
+ * 现在需要
+ * 1.重新连线(要求把破碎的线接上)
+ * 2.并输出破碎节点的顺序
  * <pre>
  *      ┌─ ──c──┐
  *   ┌─x┤       ├─m
@@ -34,13 +36,24 @@ public class CutFlowLine {
         CutFlowLine cutFlowLine = new CutFlowLine();
         List<Line> newLine = cutFlowLine.setNewLine(nodeList, lineMap);
         // 查看结果
-        Map<Integer, List<Node>> nodeCollect = nodeList.stream()
-                .collect(Collectors.groupingBy(Node::nodeId));
+        Map<Integer, Node> nodeMap = new HashMap<>();
+        for (Node node : nodeList) {
+            nodeMap.put(node.nodeId(), node);
+        }
         for (Line line : newLine) {
             System.out.printf("form: %s to: %s\n",
-                    nodeCollect.get(line.fromNodeId()).get(0).name(),
-                    nodeCollect.get(line.toNodeId()).get(0).name());
+                    nodeMap.get(line.fromNodeId()).name(),
+                    nodeMap.get(line.toNodeId()).name());
         }
+        // 通过连线结果对节点进行排序
+        List<Node> newNodeList = cutFlowLine.sortNodeByLine(nodeMap, lineList, lineMap);
+        if (newNodeList == null) {
+            System.out.println("排序未生效");
+        } else {
+            nodeList = newNodeList;
+        }
+        System.out.println("最终排序结果：");
+        nodeList.forEach(i -> System.out.print(i.name() + ","));
     }
 
     /**
@@ -71,29 +84,74 @@ public class CutFlowLine {
                     }
                 }
                 // 不存在就找下一层
-                if (!hasRel) {
-                    while (!hasRel) {
-                        List<Integer> nextLines = new ArrayList<>();
-                        Set<Integer> nextNodeIdSet = new HashSet<>(toNodeIdList);
-                        for (Integer aNodeId : nextNodeIdSet) {
-                            if (lineMap.containsKey(aNodeId)) {
-                                nextLines.addAll(lineMap.get(aNodeId));
-                            }
+                while (!hasRel) {
+                    List<Integer> nextLines = new ArrayList<>();
+                    Set<Integer> nextNodeIdSet = new HashSet<>(toNodeIdList);
+                    for (Integer aNodeId : nextNodeIdSet) {
+                        if (lineMap.containsKey(aNodeId)) {
+                            nextLines.addAll(lineMap.get(aNodeId));
                         }
-                        if (nextLines.size() == 0) break; // 最后一个节点就退出
-                        // 查询当前节点的下一节点是否存在
-                        for (Integer nextNodeId : toNodeIdList) {
-                            if (nodeIdList.contains(nextNodeId)) {
-                                // 存在则加入
-                                newLineList.add(new Line(nodeId, nextNodeId, true));
-                                hasRel = true;
-                            }
-                        }
-                        if (!hasRel) toNodeIdList = nextLines;
                     }
+                    if (nextLines.size() == 0) break; // 最后一个节点就退出
+                    // 查询当前节点的下一节点是否存在
+                    for (Integer nextNodeId : toNodeIdList) {
+                        if (nodeIdList.contains(nextNodeId)) {
+                            // 存在则加入
+                            newLineList.add(new Line(nodeId, nextNodeId, true));
+                            hasRel = true;
+                        }
+                    }
+                    if (!hasRel) toNodeIdList = nextLines;
                 }
             }
         }
         return newLineList;
+    }
+
+    /**
+     * 对破碎节点进行层次遍历
+     *
+     * @param nodeMap  Map<nodeId,Node>被扣掉后的剩余节点
+     * @param lineList 连线集合(旧)
+     * @param lineMap  Map<fromNode,List<toNode>>
+     * @return 按顺序排好的节点列表
+     */
+    private List<Node> sortNodeByLine(Map<Integer, Node> nodeMap, List<Line> lineList, Map<Integer, List<Integer>> lineMap) {
+        // 淘换连线集合找出发节点
+        Set<Integer> fromNodeIdSet = lineList.stream().map(Line::fromNodeId).collect(Collectors.toSet());
+        Set<Integer> toNodeIdSet = lineList.stream().map(Line::toNodeId).collect(Collectors.toSet());
+        fromNodeIdSet.removeAll(toNodeIdSet);
+        // 没有开始节点就直接返回
+        if (fromNodeIdSet.size() == 0) return null;
+        // 开始节点有这些
+        System.out.println("开始节点" + fromNodeIdSet);
+        // 从开始节点进行遍历
+        // 【注意：TreeMap是有序集合，里面的内容会按照key值进行排序】
+        Map<Integer, Set<Integer>> sortMap = new TreeMap<>();
+        for (int i = 0; i < lineList.size(); i++) {
+            sortMap.put(i, fromNodeIdSet);
+            Set<Integer> nextIdSet = new HashSet<>();
+            for (Integer nextId : fromNodeIdSet) {
+                if (lineMap.containsKey(nextId))
+                    nextIdSet.addAll(lineMap.get(nextId));
+            }
+            // 如果下层没有节点，就结束遍历
+            if (nextIdSet.size() == 0) break;
+            // 否则开始新的循环
+            fromNodeIdSet = nextIdSet;
+        }
+        System.out.println("排序的层次(原版)：" + sortMap);
+        Set<Integer> sortSet = new LinkedHashSet<>();
+        for (Set<Integer> mapVal : sortMap.values()) {
+            sortSet.addAll(mapVal);
+        }
+        System.out.println("最终节点顺序：" + sortSet);
+        // 将节点取出来
+        List<Node> newNodeList = new ArrayList<>();
+        for (Integer sortId : sortSet) {
+            if (nodeMap.containsKey(sortId))
+                newNodeList.add(nodeMap.get(sortId));
+        }
+        return newNodeList;
     }
 }
