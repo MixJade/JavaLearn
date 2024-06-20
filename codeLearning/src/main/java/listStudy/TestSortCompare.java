1861c6bd830d288d3860faa828492410af3dcc16
MixJade <1114185977@qq.com>
2024-06-20 20:45:39 +0800

    Create TestSortCompare.java

---
+++ codeLearning/src/main/java/listStudy/TestSortCompare.java
@@ -0,0 +1,35 @@
+package listStudy;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 测试对象列表根据某一属性值排序
+ * <p>学习排序器Comparator的使用</p>
+ *
+ * @author MixJade
+ * @since 2024-6-20 17:08:18
+ */
+public class TestSortCompare {
+    public static void main(String[] args) {
+        List<Man> manList = new ArrayList<>();
+        manList.add(new Man("张三", 12));
+        manList.add(new Man("李四", 23));
+        manList.add(new Man("王五", 5));
+        manList.add(new Man("小白", 82));
+        manList.add(new Man("老黑", 16));
+        manList.add(new Man("无名", null));
+        manList.add(new Man("白鸽", 23));
+        manList.add(new Man("兰博", 62));
+        // 引入排序器(根据年龄排序,null会排到最后)
+        Comparator<Man> comparator = Comparator.comparing(Man::age, Comparator.nullsLast(Comparator.naturalOrder()));
+        // 开始排序
+        manList.sort(comparator);
+        // 查看结果
+        manList.forEach(System.out::println);
+    }
+}
+
+record Man(String name, Integer age) {
+}
