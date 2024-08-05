ce1532e3f72746febe6c37cbf6186fea3e396553
MixJade <1114185977@qq.com>
2024-08-05 11:06:38 +0800

    update 自定义快捷方式GUI
    update 自定义快捷方式GUI

--- codeLearning/src/main/java/someUtils/GuiShortcut.java
+++ codeLearning/src/main/java/someUtils/GuiShortcut.java
@@ -11,29 +11,22 @@ import java.io.File;
  */
 public class GuiShortcut {
     public GuiShortcut() {
+        // 获取配置的值
+        MyDir[] myDirs = MyDir.values();
+        // 开始创建界面
         JFrame frame = new JFrame("S");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(200, 280);
+        frame.setSize(200, 140 + myDirs.length * 20);
         frame.setLocationRelativeTo(null); //此语句将窗口定位在屏幕的中央
-        frame.setLayout(new GridLayout(8, 1));
+        frame.setLayout(new GridLayout(myDirs.length + 1, 1));
         frame.setResizable(false); // 禁用最大化窗口
-
         JLabel label = new JLabel("自定义快捷方式", SwingConstants.CENTER);
         frame.add(label);
-
-        MyDir[] myDirs = new MyDir[]{
-                new MyDir("Python脚本", "/PythonLearn/Normal/utils/pyCmd", Color.GREEN),
-                new MyDir("Python笔记", "/PythonLearn/docs", Color.GREEN),
-                new MyDir("前端笔记", "/TsLearn/docs", Color.CYAN),
-                new MyDir("Java笔记", "/JavaLearn/docs/2023", Color.MAGENTA),
-                new MyDir("图片文件", "/MyPicture/public", Color.LIGHT_GRAY),
-                new MyDir("图片服务", "/MyPicture/script", Color.LIGHT_GRAY),
-                new MyDir("无用存档", "/mixArchive", Color.GRAY),
-        };
+        // 为每个按钮添加事件
         for (MyDir myDir : myDirs) {
-            JButton button1 = new JButton(myDir.getBtnNm());
-            button1.setBackground(myDir.getColor());
-            button1.addActionListener(e -> openDir(myDir.getDirPath()));
+            JButton button1 = new JButton(myDir.btnNm());
+            button1.setBackground(myDir.color());
+            button1.addActionListener(e -> openDir(myDir.dirPath()));
             frame.add(button1);
         }
 
@@ -48,7 +41,7 @@ public class GuiShortcut {
         if (Desktop.isDesktopSupported()) {
             try {
                 // 打开上两级文件夹
-                File myFile = new File("../../" + dirPath);
+                File myFile = new File("" + dirPath);
                 Desktop.getDesktop().open(myFile);
             } catch (Exception ex) {
                 JOptionPane.showMessageDialog(null, "目标文件夹不存在", "警告", JOptionPane.WARNING_MESSAGE);
@@ -57,26 +50,33 @@ public class GuiShortcut {
     }
 }
 
-@SuppressWarnings("ClassCanBeRecord") // 为了兼容,必须使用Java8的方式
-class MyDir {
+enum MyDir {
+    PY_CMD("Python脚本", "../../PythonLearn/Normal/utils/pyCmd", Color.GREEN),
+    PT_DOC("Python笔记", "../../PythonLearn/docs", Color.GREEN),
+    TS_DOC("前端笔记", "../../TsLearn/docs", Color.CYAN),
+    JAVA_DOC("Java笔记", "../../JavaLearn/docs/2023", Color.MAGENTA),
+    PIC_PUB("图片文件", "../../MyPicture/public", Color.LIGHT_GRAY),
+    UNUSED("无用文件", "unusedFile", Color.GRAY),
+    MIX_ARCH("备份存档", "../../mixArchive", Color.GRAY),
+    ;
     private final String btnNm, dirPath;
     private final Color color;
 
-    public MyDir(String btnNm, String dirPath, Color color) {
+    MyDir(String btnNm, String dirPath, Color color) {
         this.btnNm = btnNm;
         this.dirPath = dirPath;
         this.color = color;
     }
 
-    public String getBtnNm() {
+    public String btnNm() {
         return btnNm;
     }
 
-    public String getDirPath() {
+    public String dirPath() {
         return dirPath;
     }
 
-    public Color getColor() {
+    public Color color() {
         return color;
     }
 }
