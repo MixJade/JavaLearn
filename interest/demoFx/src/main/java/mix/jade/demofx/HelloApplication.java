e0e2d29769e92226d93476c0087fb1a42f132982
MixJade <111669273+MixJade@users.noreply.github.com>
2024-08-17 11:13:00 +0800

    JavaFx生命周期

--- interest/demoFx/src/main/java/mix/jade/demofx/HelloApplication.java
+++ interest/demoFx/src/main/java/mix/jade/demofx/HelloApplication.java
@@ -9,12 +9,25 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
+    public void init() throws Exception {
+        System.out.println("生命周期初始化阶段(没什么用)");
+        super.init();
+    }
+
+    @Override
     public void start(Stage stage) throws IOException {
+        System.out.println("生命周期启动阶段");
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 620, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
-        stage.show();
+        stage.show(); // 让窗口出现必须调show
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("生命周期结束阶段(没什么用)");
+        super.stop();
     }
 
     public static void main(String[] args) {
