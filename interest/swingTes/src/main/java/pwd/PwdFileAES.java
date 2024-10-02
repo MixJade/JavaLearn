02d34e8eef301c358401286be6ae062fcb199dff
MixJade <111669273+MixJade@users.noreply.github.com>
2024-10-02 23:15:39 +0800

    swingTes的AES代码优化

--- interest/swingTes/src/main/java/pwd/PwdFileAES.java
+++ interest/swingTes/src/main/java/pwd/PwdFileAES.java
@@ -11,21 +11,27 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+
+import static pwd.PwdAES.padIvString;
+import static pwd.PwdAES.padKeyString;
 
 /**
- * AES加密
+ * AES文件加密
  *
- * @since 2024-1-2 19:47
+ * @since 2024-10-02 22:14:47
  */
-public class PwdAES extends JFrame implements ActionListener {
+public class PwdFileAES extends JFrame implements ActionListener {
     // 文本输入与密钥输入
-    private final JTextField inputTextField, keyTextField, ivTextField;
+    private final JTextField keyTextField, ivTextField;
     // 结果输出
     JTextArea showArea;
+    private File selectedFile = null;
 
-    public PwdAES() {
+    public PwdFileAES() {
         // 使用东西南北布局
         setLayout(new BorderLayout());
         setTitle("AES加密");
@@ -34,21 +40,22 @@ public class PwdAES extends JFrame implements ActionListener {
         setIconImage(icon.getImage());
         // 上方面板
         JPanel topPanel = new JPanel();
-        topPanel.add(new JLabel("输入"));
-        inputTextField = new JTextField(24);
-        inputTextField.setText("Hello World");
-        topPanel.add(inputTextField);
+        topPanel.add(new JLabel("AES文件加密"));
         // 右边区域
         JPanel rightPanel = new JPanel();
         // 设置布局为 BoxLayout，元素竖直排列
         BoxLayout rightLayout = new BoxLayout(rightPanel, BoxLayout.Y_AXIS);
         rightPanel.setLayout(rightLayout);
         // 两个按钮
-        JButton inBtn = new JButton("AES加密"),
+        JButton openDialogBtn = new JButton("选择文件"),
+                inBtn = new JButton("AES加密"),
                 outBtn = new JButton("AES解密");
+        rightPanel.add(openDialogBtn);
         rightPanel.add(inBtn);
         rightPanel.add(outBtn);
         // 添加监听器
+        openDialogBtn.setActionCommand("CHOOSE_FILE");
+        openDialogBtn.addActionListener(this);
         inBtn.setActionCommand("AES_ENC");
         inBtn.addActionListener(this);
         outBtn.setActionCommand("AES_DEC");
@@ -109,38 +116,45 @@ public class PwdAES extends JFrame implements ActionListener {
     /**
      * 使用AES对明文进行加密
      *
-     * @param key   密钥
-     * @param iv    初始化向量
-     * @param value 明文
+     * @param key        密钥
+     * @param iv         初始化向量
+     * @param originFile 明文
      * @return 加密后的密文
      * @since 2024/1/2 15:00
      */
-    public static String encrypt(SecretKey key, IvParameterSpec iv, String value) {
-        try {
+    public static boolean encryptFile(SecretKey key, IvParameterSpec iv, File originFile) {
+        // 创建一个写入指定文件数据的文件输出流
+        try (FileOutputStream outputStream = new FileOutputStream(originFile.getPath() + ".enc")) {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
             cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-            byte[] encrypted = cipher.doFinal(value.getBytes());
-            return Base64.getEncoder().encodeToString(encrypted);
+            String fileContext = Files.readString(originFile.toPath());
+            System.out.println(fileContext);
+            byte[] encrypted = cipher.doFinal(fileContext.getBytes());
+            // 把二进制数据写入文件
+            outputStream.write(encrypted);
+
+            System.out.println("Binary data has been written to file.");
+            return true;
         } catch (Exception e) {
             e.printStackTrace();
-            return "加密失败";
+            return false;
         }
     }
 
     /**
      * 对AES的密文进行解密
      *
-     * @param key       密钥
-     * @param iv        初始化向量
-     * @param encrypted 密文
+     * @param key     密钥
+     * @param iv      初始化向量
+     * @param encFile 密文
      * @return 明文
      * @since 2024/1/2 15:00
      */
-    public static String decrypt(SecretKey key, IvParameterSpec iv, String encrypted) {
-        try {
+    public static String decryptFile(SecretKey key, IvParameterSpec iv, File encFile) {
+        try (FileInputStream inputStream = new FileInputStream(encFile)) {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, key, iv);
-            byte[] original = cipher.doFinal(Base64.getDecoder().decode(encrypted));
+            byte[] original = cipher.doFinal(inputStream.readAllBytes());
             return new String(original);
         } catch (Exception e) {
             e.printStackTrace();
@@ -150,100 +164,50 @@ public class PwdAES extends JFrame implements ActionListener {
 
     public static void main(String[] args) {
         FlatIntelliJLaf.setup();
-        new PwdAES();
+        new PwdFileAES();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if ("CHOOSE_FILE".equals(e.getActionCommand())) {
+            // 打开选择文件对话框
+            JFileChooser fileChooser = new JFileChooser("../../");
+            fileChooser.setDialogTitle("请选择待加解密的文件");
+            // 打开一个文件对话框
+            int returnValue = fileChooser.showOpenDialog(null);
+            // 当用户选中一个文件并点击"Open"按钮时，returnValue的值为 `JFileChooser.APPROVE_OPTION`
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                // 用户选中的文件
+                selectedFile = fileChooser.getSelectedFile();
+                showArea.setText("选中的文件名：" + selectedFile.getName() + "\n" + selectedFile.getPath());
+            }
+            return;
+        }
         // 处理密钥
         SecretKey key = new SecretKeySpec(padKeyString(keyTextField.getText()), "AES");
-        // System.out.println(new String(padKeyString(keyTextField.getText()),StandardCharsets.UTF_8));
         // 处理向量
         IvParameterSpec iv = new IvParameterSpec(padIvString(ivTextField.getText()));
-        // System.out.println(new String(padIvString(ivTextField.getText()),StandardCharsets.UTF_8));
-        // 处理输入文本
-        String inputText = inputTextField.getText();
         // 进行加密、解密
+        if (selectedFile == null) {
+            JOptionPane.showMessageDialog(null, "请先选择文件", "散玉说", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
         if ("AES_ENC".equals(e.getActionCommand())) {
             // 加密
-            showArea.setText(encrypt(key, iv, inputText));
+            boolean encryptSuc = encryptFile(key, iv, selectedFile);
+            if (encryptSuc)
+                showArea.setText("文件加密成功:" + selectedFile.getPath() + ".enc");
+            else
+                showArea.setText("文件加密失败");
         } else if ("AES_DEC".equals(e.getActionCommand())) {
             // 解密
-            showArea.setText(decrypt(key, iv, inputText));
+            showArea.setText(decryptFile(key, iv, selectedFile));
         } else {
             // 重置
             showArea.setText("");
+            selectedFile = null;
             keyTextField.setText("");
             ivTextField.setText("");
-        }
-    }
-
-    /**
-     * 对输入的密钥进行处理，输出长度为16、24、32的数组
-     *
-     * @param input 输入的密钥文本
-     * @return 长度为16、24、32的byte[]
-     * @since 2024/1/2 15:00
-     */
-    public static byte[] padKeyString(String input) {
-        byte[] inputBytes = input.getBytes(StandardCharsets.UTF_8);
-        if (inputBytes.length < 24) {
-            // 输出的数组长度小于24，则输出16位的密钥
-            return padIvString(input, "WhatCannotBeSeen");
-        } else if (inputBytes.length > 24 && inputBytes.length < 32) {
-            // 输出数组的长度大于24但小于32，则输出24位数组
-            byte[] result = new byte[24];
-            // 将inputBytes前24位给复制到result中
-            System.arraycopy(inputBytes, 0, result, 0, 24);
-            return result;
-        } else if (inputBytes.length > 32) {
-            // 输出数组的长度大于32，则输出32位数组
-            byte[] result = new byte[32];
-            System.arraycopy(inputBytes, 0, result, 0, 32);
-            return result;
-        } else {
-            // 刚好就是24、32的长度
-            return inputBytes;
-        }
-    }
-
-    /**
-     * 重置方法：对输入的向量进行处理
-     *
-     * @param input 输入的向量文本
-     * @return 16位的byte数组
-     */
-    public static byte[] padIvString(String input) {
-        return padIvString(input, "TimeWaitForNoMan");
-    }
-
-    /**
-     * 对输入的向量进行处理，保证输出16位的byte数组
-     *
-     * @param input  输入的向量文本
-     * @param defTxt 默认填充文本
-     * @return 16位的byte数组
-     * @since 2024/1/2 15:00
-     */
-    public static byte[] padIvString(String input, String defTxt) {
-        byte[] padBytes = defTxt.getBytes(StandardCharsets.UTF_8);
-        byte[] inputBytes = input.getBytes(StandardCharsets.UTF_8);
-        if (inputBytes.length < 16) {
-            // 输出的数组长度小于16
-            byte[] result = new byte[16];
-            // 将inputBytes全部复制到result中
-            System.arraycopy(inputBytes, 0, result, 0, inputBytes.length);
-            // 将填空字符串拼接到输出字符串中
-            System.arraycopy(padBytes, inputBytes.length, result, inputBytes.length, 16 - (inputBytes.length));
-            return result;
-        } else if (inputBytes.length > 16) {
-            // 输出数组的长度大于16
-            byte[] result = new byte[16];
-            // 将inputBytes前16位给复制到result中
-            System.arraycopy(inputBytes, 0, result, 0, 16);
-            return result;
-        } else {
-            return inputBytes;
         }
     }
 }
