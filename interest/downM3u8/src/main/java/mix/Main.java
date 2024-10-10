ef64fdf8739fbaebb4a9c33f63d16253d3701855
MixJade <1114185977@qq.com>
2024-10-10 15:38:22 +0800

    下载m3u8的第一个面板

--- interest/downM3u8/src/main/java/mix/Main.java
+++ interest/downM3u8/src/main/java/mix/Main.java
@@ -1,7 +1,49 @@
 package mix;
 
-public class Main {
+import mix.show.Panel1;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main extends JFrame {
+    Main() {
+        // 创建一个新的 JFrame
+        setTitle("m3u8下载器");
+        // 设置图标
+        try (InputStream favor = getClass().getResourceAsStream("th023.jpg")) {
+            if (favor != null) {
+                ImageIcon icon = new ImageIcon(ImageIO.read(favor));
+                setIconImage(icon.getImage());
+            }
+        } catch (IOException e) {
+            System.out.println("未读到图片");
+        }
+        // 创建一个新的 JTabbedPane
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        // 创建面板1
+        JPanel panel1 = new Panel1();
+        tabbedPane.addTab("基本参数", panel1);
+
+        // 创建面板2
+        JPanel panel2 = new JPanel();
+        panel2.add(new JLabel("这 is 面板2"));
+        tabbedPane.addTab("下载ts", panel2);
+
+        // 将选项卡面板添加到帧中
+        add(tabbedPane);
+
+        // 设置窗口为最佳大小
+        pack();
+        // 启动窗口
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main();
     }
 }
---
+++ interest/downM3u8/src/main/java/mix/show/Panel1.java
@@ -0,0 +1,82 @@
+package mix.show;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.nio.file.Paths;
+
+/**
+ * 下载M3u8以及设置一些基础参数
+ */
+public class Panel1 extends JPanel implements ActionListener {
+    private final JTextField m3u8Url, savePath, m3u8Name;
+    private final JButton saveBtn;
+
+    public Panel1() {
+        // 设置布局
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        setLayout(gridBagLayout);
+        GridBagConstraints gbc = new GridBagConstraints();//实例化这个对象用来对组件进行管理
+        gbc.fill = GridBagConstraints.NONE; // 如果组件所在的区域比组件大,不调整组件大小。
+        // 加入组件
+        // 组件1(grid-x,grid-y)组件的左上角坐标
+        // grid-width，grid-height：组件占用的网格行数和列数
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.gridheight = 1;
+        add(new JLabel("m3u8地址"), gbc);
+        // 对输入框进行设置
+        m3u8Url = new JTextField("http://127.0.0.1/sss/index.m3u8", 20);
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        gbc.gridwidth = 10;
+        gbc.gridheight = 1;
+        add(m3u8Url, gbc);
+        // 保存地址lab
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        gbc.gridheight = 1;
+        add(new JLabel("保存文件夹"), gbc);
+        // 保存地址输入框
+        savePath = new JTextField("example/sss", 20);
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        gbc.gridwidth = 10;
+        gbc.gridheight = 1;
+        add(savePath, gbc);
+        // m3u8文件名
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.gridheight = 1;
+        add(new JLabel("m3u8文件名"), gbc);
+        // 保存地址输入框
+        m3u8Name = new JTextField("index.m3u8", 20);
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        gbc.gridwidth = 10;
+        gbc.gridheight = 1;
+        add(m3u8Name, gbc);
+        // 下载按钮
+        saveBtn = new JButton("下载m3u8");
+        gbc.gridx = 2;
+        gbc.gridy = 3;
+        gbc.gridwidth = 2;
+        gbc.gridheight = 1;
+        saveBtn.setActionCommand("SAVE_BTN");
+        saveBtn.addActionListener(this);
+        add(saveBtn, gbc);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if ("SAVE_BTN".equals(e.getActionCommand())) {
+            saveBtn.setEnabled(false); // 禁止多次点击
+            System.out.println("下载地址:" + m3u8Url.getText());
+            System.out.println("转存文件名:" + Paths.get(savePath.getText(), m3u8Name.getText()));
+        }
+    }
+}
