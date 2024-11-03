2b7994fd58d1213af7b2638ee4ed20f884ad7337
MixJade <111669273+MixJade@users.noreply.github.com>
2024-11-03 16:19:13 +0800

    聊天页面优化

--- experiment/chat/src/main/java/com/chat/ChatApplication.java
+++ experiment/chat/src/main/java/com/chat/ChatApplication.java
@@ -1,14 +1,26 @@
 package com.chat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class ChatApplication {
+    private static final Logger log = LoggerFactory.getLogger(ChatApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
-        System.out.println("http://localhost:8080");
+        log.info("本地IP: http://localhost:23042");
+        try {
+            InetAddress ip01 = InetAddress.getLocalHost();
+            //noinspection HttpUrlsUsage
+            log.info("局域IP: http://{}:23042", ip01.getHostAddress());
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
     }
-
 }
--- experiment/chat/src/main/java/com/chat/controller/LoginController.java
+++ experiment/chat/src/main/java/com/chat/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.chat.controller;
 
 import com.chat.pojo.Result;
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+
+    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+
     @GetMapping("/toLogin")
     public Result toLogin(@RequestParam("user") String user, HttpSession session) {
+        log.info("用户【{}】登录", user);
         if (user != null && !user.isBlank()) {
             session.setAttribute("user", user);
             return new Result(true, "成功");
@@ -21,8 +27,8 @@ public class LoginController {
 
     @GetMapping("/getUsername")
     public String getUsername(HttpSession session) {
-        String username=(String) session.getAttribute("user");
-        if (username==null) return "noMan";
+        String username = (String) session.getAttribute("user");
+        if (username == null) return "noMan";
         return username;
     }
 }
