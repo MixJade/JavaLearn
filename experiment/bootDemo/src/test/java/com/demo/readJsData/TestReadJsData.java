80765f4f54eeeb13fcfdd3024deabcff3449534c
MixJade <111669273+MixJade@users.noreply.github.com>
2024-11-24 19:23:59 +0800

    测试使用jackson读取js文件中的json

---
+++ experiment/bootDemo/src/test/java/com/demo/readJsData/TestReadJsData.java
@@ -0,0 +1,70 @@
+package com.demo.readJsData;
+
+import com.fasterxml.jackson.core.json.JsonReadFeature;
+import com.fasterxml.jackson.core.json.JsonWriteFeature;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 测试使用JackSon读取js文件中的json
+ *
+ * @since 2024-11-24 19:23:38
+ */
+public class TestReadJsData {
+    public static void main(String[] args) throws IOException {
+        String string = Files.readString(Path.of("src/test/java/com/demo/readJsData/测试的JS文件.js"));
+        // System.out.println(string);
+        Pattern p = Pattern.compile("(?<=const myData = )([\\s\\S]*?)(?=;)");
+        Matcher m = p.matcher(string);
+        while (m.find()) {
+            String group = m.group();
+            // System.out.println(group);
+            // 配置参见 https://blog.csdn.net/u013066244/article/details/119028446
+            ObjectMapper objectMapper = JsonMapper.builder()
+                    // 设为false: 转string属性名称不带双引号
+                    .disable(JsonWriteFeature.QUOTE_FIELD_NAMES)
+                    // 反序列化是是否允许属性名称不带双引号
+                    .enable(JsonReadFeature.ALLOW_UNQUOTED_FIELD_NAMES)
+                    // 支持结尾逗号
+                    .enable(JsonReadFeature.ALLOW_TRAILING_COMMA)
+                    .build();
+            List<DataGroup> dataGroup = objectMapper.readValue(group, new TypeReference<>() {
+            });
+            System.out.println(dataGroup.size());
+            System.out.println(objectMapper.writeValueAsString(dataGroup));
+        }
+    }
+}
+
+@SuppressWarnings("unused")
+class DataGroup {
+    public String longNav;
+    public AGroup mainBox;
+    public AGroup bigSide;
+    public AGroup bottom1;
+    public AGroup bottom2;
+    public AGroup bottom3;
+    public String updateTime;
+}
+
+@SuppressWarnings("unused")
+class AGroup {
+    public String name;
+    public List<DefA> ul;
+}
+
+@SuppressWarnings("unused")
+class DefA {
+    public String href;
+    public String name;
+    public String remark;
+    public String pwd;
+}
---
+++ experiment/bootDemo/src/test/java/com/demo/readJsData/测试的JS文件.js
@@ -0,0 +1,108 @@
+(() => {
+    const myData = [{
+        longNav: "初始教程",
+        mainBox: {
+            name: "参考网站",
+            ul: [{
+                href: "https://cn.vuejs.org/guide/quick-start.html",
+                name: "Vue上手",
+                remark: "一切根基",
+                pwd: "测试复制"
+            }, {
+                href: "https://element-plus.org/zh-CN/",
+                name: "Element-Plus",
+                remark: "项目组件库"
+            }, {
+                href: "https://element-plus.org/zh-CN/component/icon.html",
+                name: "Element的Icon",
+                remark: "美化图标"
+            },]
+        },
+        bigSide: {
+            name: "样式参考",
+            ul: [{
+                href: "https://www.sass.hk/docs/",
+                name: "Sass官网教程",
+                remark: "但不建议看这个"
+            }, {
+                href: "https://www.runoob.com/sass/sass-install.html",
+                name: "Sass菜鸟教程",
+                remark: "这个写的不错"
+            }, {
+                href: "https://www.bootstrap.cn/doc/read/113.html",
+                name: "bootstrap官网",
+                remark: "一些样式要从这里来"
+            },]
+        },
+        bottom1: {
+            name: "框架官网",
+            ul: [{
+                href: "https://www.axios-http.cn/docs/intro", name: "Axios官网", remark: "前端请求"
+            }, {
+                href: "https://zh-hans.eslint.org/docs/latest/use/getting-started",
+                name: "ESLint官网",
+                remark: "格式化工具"
+            },
+            ]
+        },
+        bottom2: {name: "", ul: []},
+        bottom3: {name: "", ul: []},
+        updateTime: "2024-6-11 16:32"
+    }, {
+        longNav: "补充知识",
+        mainBox: {
+            name: "前端知识", ul: [{
+                href: "https://blog.csdn.net/qq_42231156/article/details/124506627",
+                name: "计算属性传参",
+                remark: ""
+            }, {
+                href: "https://blog.csdn.net/GraySakura/article/details/104821218",
+                name: "div换行设置",
+                remark: ""
+            }, {
+                href: "https://juejin.cn/post/7074448287352225823",
+                name: "Vue3关于路由动画的注意事项",
+                remark: ""
+            }, {
+                href: "https://juejin.cn/post/7137460519497105439",
+                name: "Vue3关于滚动条的细节",
+                remark: "包括flex翻转，用做聊天页面"
+            }, {href: "https://juejin.cn/post/7132656370901336101", name: "Vue3的h函数", remark: ""},]
+        },
+        bigSide: {
+            name: "样式知识",
+            ul: [{
+                href: "https://developer.mozilla.org/zh-CN/docs/Web/CSS/Using_CSS_custom_properties",
+                name: "CSS自定属性",
+                remark: "方便调配色"
+            }, {
+                href: "https://www.jianshu.com/p/d875f680fc6b",
+                name: "CSS波浪加号大于号",
+                remark: "一个有用的语法"
+            }, {
+                href: "https://zhuanlan.zhihu.com/p/96721026",
+                name: "前端尺寸vw|vh|rem|em",
+                remark: "了解即可"
+            },]
+        },
+        bottom1: {
+            name: "后端",
+            ul: [{
+                href: "https://blog.csdn.net/hunt_er/article/details/81427804",
+                name: "无Java环境下运行jar文件",
+                remark: "感觉不如IDE"
+            }]
+        },
+        bottom2: {
+            name: "别人仓库",
+            ul: [{
+                href: "https://gitee.com/haojunchuan/web_every_day/tree/master/parallax_website",
+                name: "别人的仓库",
+                remark: "我会吸收一些东西"
+            }]
+        },
+        bottom3: {name: "", ul: []},
+        updateTime: "2024-6-11 17:52"
+    }];
+    console.log(myData)
+})();
