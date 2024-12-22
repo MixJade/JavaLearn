0111f8143f78a65eb872a0187d9d6b780169c9b9
MixJade <111669273+MixJade@users.noreply.github.com>
2024-12-22 22:08:14 +0800

    账本分页优化

--- experiment/payment/src/main/resources/static/assert/pageNav.js
+++ experiment/payment/src/main/resources/static/assert/pageNav.js
@@ -1,25 +1,32 @@
 const paSize = $("paSize") // 调整大小的选择框
-const pageStrip = $("pageStrip") // 分页条(存储页码)
-const dataNumSpan = $("dataNum") // 分页条(存储页码)
+const paNum = $("paNum") // 分页条(存储页码)
+const dataNumSpan = $("dataNum") // 数据长度展示
 let nowPage = 1; // 当前页码
+let pageNumMax = 1; // 当前页码最大值
 /**
  * 初始化分页条
  * @param dataNum 数据长度
  * @param pageNum 页码数
  */
 const firstLoadPa = (dataNum, pageNum) => {
-    pageStrip.innerHTML = ''
     dataNumSpan.innerText = dataNum
-    for (let i = 1; i < pageNum + 1; i++) {
-        pageStrip.innerHTML += `<span onclick="cutPage(${i})">${i}</span>`
-    }
-    // 选中指定子元素
-    const paOneList = pageStrip.getElementsByTagName("span");
-    paOneList[nowPage - 1].classList.add("active")
+    pageNumMax = pageNum
+    paNum.innerHTML = `<span onclick="jumpPage(${pageNum})">${nowPage}</span>`
 }
 
 /**
- * 点击页码操作(也是分页条核心方法)
+ * 点击页码的输入框
+ * @param pageNum 页码最大值
+ */
+const jumpPage = (pageNum) => {
+    let num = prompt(`输入要跳转页码(最大${pageNum})`, "1");
+    // 提示用户输入一个数字，如果用户不输入任何内容，输入字段默认会显示"1"
+    num = Number(num); // 将输入的字符串转换为数字格式
+    cutPage(num)
+}
+
+/**
+ * 跳转页码操作(也是分页条核心方法)
  * @param paNum 当前页码的索引
  */
 const cutPage = (paNum) => {
@@ -37,7 +44,7 @@ const addPaNum = (isLeft) => {
         if (nowPage === 1) return;
         cutPage(nowPage - 1)
     } else {
-        if (nowPage === pageStrip.getElementsByTagName("span").length) return;
+        if (nowPage === pageNumMax) return;
         cutPage(nowPage + 1)
     }
 }
