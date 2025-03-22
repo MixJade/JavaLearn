8d569af53d7f21f1a7507c94b42fc444957f2977
MixJade <111669273+MixJade@users.noreply.github.com>
2025-03-22 23:10:41 +0800

    Update TestCircleUpdate.java

--- codeLearning/src/test/java/testCircle/TestCircleUpdate.java
+++ codeLearning/src/test/java/testCircle/TestCircleUpdate.java
@@ -16,10 +16,37 @@ public class TestCircleUpdate {
         needUpdates.add(new NeedUpdate(3, "你好"));
         for (NeedUpdate needUpdate : needUpdates) {
             if (needUpdate.getNumber() != 3) {
-                needUpdate.setMsg("已更改");
+                changeVal(needUpdate, "二等兵" + needUpdate.getNumber());
             }
         }
+        System.out.println("在列表中循环可以被更改：");
         System.out.println(needUpdates); // 能够更改
+
+        System.out.println("在其它类中也可以被更改：");
+        ChangeNed changeNed = new ChangeNed(needUpdates.get(1));
+        changeNed.setNeedUpdateStr("这个LuLu在其它类中被改");
+        System.out.println(needUpdates); // 在其它类中,也能够更改
+    }
+
+    /**
+     * 改变对象的值
+     *
+     * @param replaceStr 替换值
+     */
+    private static void changeVal(NeedUpdate needUpdate, String replaceStr) {
+        needUpdate.setMsg(replaceStr);
+    }
+}
+
+class ChangeNed {
+    private final NeedUpdate needUpdate;
+
+    public ChangeNed(NeedUpdate needUpdate) {
+        this.needUpdate = needUpdate;
+    }
+
+    public void setNeedUpdateStr(String msg) {
+        this.needUpdate.setMsg(msg);
     }
 }
 
