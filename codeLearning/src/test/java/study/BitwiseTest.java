9c7fffed1a78877b542b6818c03c2b60eb562301
MixJade <111669273+MixJade@users.noreply.github.com>
2025-03-23 10:32:27 +0800

    Update BitwiseTest.java

--- codeLearning/src/test/java/study/BitwiseTest.java
+++ codeLearning/src/test/java/study/BitwiseTest.java
@@ -7,9 +7,9 @@ package study;
  */
 public class BitwiseTest {
     public static void main(String[] args) {
-        System.out.println("2的9次方 : " + (2 << 9) + " 预期:1024");
-        System.out.println("2的19次方: " + (2 << 19) + " 预期:" + (1024 * 1024));
-        System.out.println("2的29次方: " + (2 << 29) + " 预期:" + (1024 * 1024 * 1024));
+        System.out.println("2的10次方 : " + (1 << 10) + " 预期:1024");
+        System.out.println("2的20次方: " + (1 << 20) + " 预期:" + (1024 * 1024));
+        System.out.println("2的30次方: " + (1 << 30) + " 预期:" + (1024 * 1024 * 1024));
         // 开始计算
         System.out.println("10240除1024: " + (10240 >> 10) + " 预期:10");
         // 位运算保留小数
