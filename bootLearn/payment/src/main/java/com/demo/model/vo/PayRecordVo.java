da82eb991df750c399db32a6de8c2073c799bff8
MixJade <111669273+MixJade@users.noreply.github.com>
2025-03-29 23:29:04 +0800

    记账：分页查询字段优化

--- bootLearn/payment/src/main/java/com/demo/model/vo/PayRecordVo.java
+++ bootLearn/payment/src/main/java/com/demo/model/vo/PayRecordVo.java
@@ -11,9 +11,9 @@ public class PayRecordVo extends PaymentRecord {
     private String keyName;
 
     /**
-     * 分类颜色
+     * 字典大类,存于代码
      */
-    private String color;
+    private Integer bigType;
 
     public String getKeyName() {
         return keyName;
@@ -23,11 +23,11 @@ public class PayRecordVo extends PaymentRecord {
         this.keyName = keyName;
     }
 
-    public String getColor() {
-        return color;
+    public Integer getBigType() {
+        return bigType;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+    public void setBigType(Integer bigType) {
+        this.bigType = bigType;
     }
 }
