b83d63cf235e111f8236579a4daeb44e8b735436
MixJade <1114185977@qq.com>
2025-11-26 10:50:59 +0800

    Update MyConfig.java

--- libLearn/genCodeUtil/src/main/java/MyConfig.java
+++ libLearn/genCodeUtil/src/main/java/MyConfig.java
@@ -3,19 +3,14 @@ interface MyConfig {
      * 需要生成的表名(设为空即为生成所有表)
      */
     String[] tableNames = new String[]{"source_category", "source_image", "source_image"};
-    /**
-     * 项目名称、数据库名称
-     */
-    String prjName = "prep",
-            dbName = "prep";
 
-    // ================================[以下配置不用改]================================
     // 数据库链接、账号、密码
-    String url = "jdbc:mysql://localhost:3306/" + dbName + "?useSSL=true",
+    // 注意数据库名称
+    String url = "jdbc:mysql://localhost:3306/prep?useSSL=true",
             username = "root",
             password = "MC@:(==ni2024";
     // 输出目录的路径，到项目名称为止
-    String outputDir = "../../bootLearn/" + prjName;
+    String outputDir = "../../bootLearn/prep";
     // 软件包、entity文件的包路径
     String parentPack = "com.demo",
             entityPack = "model.entity",
