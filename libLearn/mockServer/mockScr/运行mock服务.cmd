eeff4383909a61e8fc49decb8fc1d64f926a08ac
MixJade <1114185977@qq.com>
2026-06-08 11:18:38 +0800

    Update 运行mock服务.cmd

--- libLearn/mockServer/mockScr/运行mock服务.cmd
+++ libLearn/mockServer/mockScr/运行mock服务.cmd
@@ -6,6 +6,8 @@ cd ..
 md target\classes 2>nul
 :: 编译java 文件
 javac -cp . -d target\classes -encoding UTF-8 src\main\java\*.java src\main\java\mock\*.java
+:: 复制resources资源文件到classes目录
+xcopy /S /Y "src\main\resources\*" "target\classes\" >nul
 :: 运行demo
 java -Dfile.encoding=UTF-8 -classpath target\classes MockDemo
 pause
